a4b58bab837c5f99c59b46644aff89e358cf5845
SaayanBiswas98 <76898125+SaayanBiswas98@users.noreply.github.com>
2021-01-03 15:29:11 +0530

    File uploaded

---
+++ pal.php
@@ -0,0 +1,45 @@
+<?php
+function palindrome($number){
+    
+$temp=$number;
+    
+$new=0;
+    
+while(floor($temp)){
+        
+$d=$temp%10;
+        
+$new=$new*10+$d;
+        
+$temp=$temp/10;
+    
+}
+    
+if($new==$number){
+       
+return 1;
+    
+}
+    
+else{
+        
+return 0;
+    
+}
+}
+    
+$real=67789;
+    
+if (palindrome($real)){
+   
+    echo"Palindrome";
+  
+  }
+    
+else{
+    
+echo"non Palindrome";
+   
+ }
+    
+?>
